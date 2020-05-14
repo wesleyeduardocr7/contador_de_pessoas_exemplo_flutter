43925d5e677733e82bd4d69bbb62496ddedb6a17
wesleyeduardocr7 <wesley.eduardo@acad.ifma.edu.br>
2020-05-14 08:45:55 -0300

    finalcommit

---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: "Contador de Pessoas",
+      home: Home()));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  int _people = 8;
+
+  String _infoTest = "Pode Entrar";
+
+  void changePeople(int delta){
+
+      setState(() {
+        _people+=delta;
+
+        if(_people < 0){
+          _infoTest = "Mundo Invertido?!";
+        }else if ( _people <= 10 ){
+          _infoTest = "Pode Entrar";
+        }else{
+          _infoTest = "Lotado";
+        }
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+
+        Image.asset(
+          "images/original.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+
+        Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: <Widget>[
+            Text(
+                "Pessoas: $_people",
+                style:TextStyle(color: Colors.white, fontWeight: FontWeight.bold)
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child:
+                  FlatButton(
+                    child: Text("+1", style: TextStyle(fontSize: 40.0, color: Colors.white)),
+                    onPressed: (){
+                     changePeople(1);
+                    },
+                  ),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child:
+                  FlatButton(
+                    child: Text("-1", style: TextStyle(fontSize: 40.0, color: Colors.white)),
+                    onPressed: (){
+                      changePeople(-1);
+                    },
+                  ),
+                ),
+              ],
+            ),
+
+            Text(
+              _infoTest,
+              style:TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0
+              ),
+            )
+          ],
+        )
+
+      ],
+
+    );
+  }
+}
